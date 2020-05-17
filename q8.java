08257b6f75f9fba3e8d28813adf36c3ed2d495c8
https://github.com/Tanish2106/10-D-Programs/blob/master/README.TXT <tanishanand2106@gmail.com>
2020-05-17 15:44:08 +0530

    17-05-2020

---
+++ q8.java
@@ -0,0 +1,32 @@
+public class q8
+{
+    void check(String str, char ch)
+    {
+        int count=0;
+        for(int i=0;i<str.length();i++)
+        {
+            if(str.charAt(i)==ch)
+            {
+                count++;
+            }
+        }
+            System.out.print("The number of "+ch);
+            System.out.println(" present is " + count);
+     }
+    void check(String s1)
+    {
+        s1=s1.toLowerCase();
+        char[]vowels={'a', 'e', 'i', 'o', 'u'};
+        for(int i=0;i<s1.length();i++)
+        {
+            for(int j=0;j<vowels.length;j++)
+            {
+                if(s1.charAt(i)==vowels[j])
+                {
+                    System.out.print(vowels[j]);
+                }
+            }
+        }
+    }
+}
+                
